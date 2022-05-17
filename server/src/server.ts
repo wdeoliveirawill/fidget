5e8ee95fccf641d54fc8c624d598b29a37f15286
Will <william.oliveira@coteminas.com.br>
2022-05-16 22:02:38 -0300

    request size limit error

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,9 @@ const app = express();
 app.use(cors({
   origin: '*'
 }));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
